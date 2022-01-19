43f4e6e22ba706b359c27847e31f42c0b47815db
spike04 <rubinbajracharya@yahoo.com>
2022-01-19 14:26:33 +0545

    Initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,22 @@
+<div class="about">
+  <h2>About us</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam
+    mollitia quisquam! Adipisci nobis enim quia consequatur tenetur voluptatibus
+    aspernatur molestias sit, aliquid minus aliquam, voluptates delectus beatae
+    nam culpa voluptas provident explicabo, reiciendis dolorem eaque totam dicta
+    doloremque. Incidunt aliquid deleniti corrupti facere expedita qui molestias
+    labore eum ullam esse eveniet nisi quia, optio aspernatur maxime et sed
+    autem.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum doloribus,
+    sequi corporis soluta perspiciatis ab expedita dolore quas obcaecati eius
+    veniam maiores quaerat officiis doloremque dolorum officia et placeat
+    consequatur illum ad aliquid deserunt ratione facilis. Porro facere est
+    suscipit ipsum unde eligendi qui similique debitis sit eius nihil, quia
+    molestias blanditiis odio natus dicta ex voluptatum earum quod reiciendis.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
